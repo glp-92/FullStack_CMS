244bba97acf16684f4bae3aab40ee5aad06e57fd
glp-92 <guillermolopezpazos@gmail.com>
2024-12-10 20:46:43 +0100

    linted code

--- backend/filestorage-service/app/main.py
+++ backend/filestorage-service/app/main.py
@@ -6,18 +6,21 @@ from fastapi.responses import PlainTextResponse
 from fastapi import FastAPI, Request, HTTPException
 from fastapi import FastAPI
 import os
+
 # from dotenv import load_dotenv
 # load_dotenv("./.env")
 
-allowed_origins = [os.getenv('FRONTEND_URL', 'http://localhost:3000')]
+allowed_origins = [os.getenv("FRONTEND_URL", "http://localhost:3000")]
 allowed_referers = allowed_origins
 
+
 async def verify_referer(request: Request):
-    referer = request.headers.get('referer')
+    referer = request.headers.get("referer")
     if not referer:
         raise HTTPException(status_code=403, detail="Forbidden")
     if not any(referer.startswith(allowed_ref) for allowed_ref in allowed_referers):
         raise HTTPException(status_code=403, detail="Forbidden")
+
 
 app = FastAPI()
 
@@ -29,16 +32,15 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+
 @app.middleware("http")
 async def custom_origin_middleware(request: Request, call_next):
     try:
         await verify_referer(request)
         return await call_next(request)
     except HTTPException as ex:
-        return PlainTextResponse(
-            status_code=ex.status_code,
-            content=ex.detail
-        )
+        return PlainTextResponse(status_code=ex.status_code, content=ex.detail)
 
-static_folder = os.getenv('FILE_STORAGE_STATIC_FOLDER', '../images')
+
+static_folder = os.getenv("FILE_STORAGE_STATIC_FOLDER", "/images")
 app.mount("/static", StaticFiles(directory=static_folder), name="static")
